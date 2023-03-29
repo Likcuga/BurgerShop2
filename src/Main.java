d38b54a11013d994ee88a5ca34833cae9510b25b
Likcuga <petarzdravkovic@gmail.com>
2023-03-29 17:21:27 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        MealOrder regularMeal = new MealOrder();
+        regularMeal.addBurgerToppings("BACON","CHEESE", "MAYO");
+        regularMeal.setDrinkSize("LARGE");
+        regularMeal.printItemizedList();
+
+
+
+    }
+}
